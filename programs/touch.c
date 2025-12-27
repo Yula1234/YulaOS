c955aefa56c16b67d66218cc1d317d8b1c0de695
Yula1234 <vpervuhina0@gmail.com>
2025-12-28 02:26:40 +0500

    touch command

---
+++ programs/touch.c
@@ -0,0 +1,30 @@
+#include <yula.h>
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        printf("Usage: touch <file>...\n");
+        return 1;
+    }
+
+    for (int i = 1; i < argc; i++) {
+        const char* path = argv[i];
+
+        int fd = open(path, 0);
+
+        if (fd >= 0) {
+            close(fd);
+        } else {
+            fd = open(path, 1);
+            
+            if (fd >= 0) {
+                close(fd);
+            } else {
+                set_console_color(0xF44747, 0x141414);
+                printf("touch: cannot create '%s'\n", path);
+                set_console_color(0xD4D4D4, 0x141414);
+            }
+        }
+    }
+
+    return 0;
+}
