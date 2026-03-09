37d7e931531cf4a7100dd2fd2ca129e0dddd3fe3
Yula1234 <151495538+Yula1234@users.noreply.github.com>
2026-03-10 01:41:37 +0500

    kernel: rust: global allocator

--- src/rust/lib.rs
+++ src/rust/lib.rs
@@ -1,5 +1,9 @@
 #![no_std]
 
+extern crate alloc;
+
+use core::alloc::{GlobalAlloc, Layout};
+
 use core::panic::PanicInfo;
 
 pub mod libc;
@@ -8,8 +12,22 @@ pub mod fs;
 pub mod hal;
 pub mod mm;
 
+struct YulaAllocator;
+
+unsafe impl GlobalAlloc for YulaAllocator {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        crate::mm::heap::kmalloc_aligned(layout.size(), layout.align() as u32) as *mut u8
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
+        crate::mm::heap::kfree(ptr as *mut core::ffi::c_void);
+    }
+}
+
+#[global_allocator]
+static ALLOCATOR: YulaAllocator = YulaAllocator;
+
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop {
-    }
+    loop { }
 }
