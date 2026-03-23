d4c6574fe332c01c4a273cd0ce8767a7998f5882
Yula1234 <151495538+Yula1234@users.noreply.github.com>
2026-03-23 21:22:58 +0500

    drivers: new pc speaker driver

--- src/drivers/pc_speaker.c
+++ src/drivers/pc_speaker.c
@@ -1,51 +1,114 @@
 // SPDX-License-Identifier: GPL-2.0
-// Copyright (C) 2025 Yula1234
+// Copyright (C) 2026 Yula1234
 
-#include "pc_speaker.h"
-#include <hal/io.h>
+#include <drivers/pc_speaker.h>
 
-static void simple_wait(int count) {
-    for (volatile int i = 0; i < count * 10000; i++);
+#include <hal/pmio.h>
+#include <lib/compiler.h>
+
+#define PIT_FREQUENCY      1193180u
+
+#define PORT_PIT_CH2       0x42u
+#define PORT_PIT_COMMAND   0x43u
+#define PORT_PPI_CTRL      0x61u
+
+#define PIT_CMD_CH2_SQUARE 0xB6u
+
+#define PPI_SPEAKER_ENABLE 0x03u
+#define PPI_SPEAKER_MASK   0xFCu
+
+static pmio_region_t* g_pit_ch2_region = 0;
+static pmio_region_t* g_ppi_ctrl_region = 0;
+
+static int g_speaker_ready = 0;
+
+static void speaker_delay(uint32_t units) {
+    const uint32_t iterations = units * 10000u;
+
+    for (volatile uint32_t i = 0u; i < iterations; i++) { }
+}
+
+
+void pc_speaker_init(void) {
+    if (unlikely(g_speaker_ready)) {
+        return;
+    }
+
+    g_pit_ch2_region = pmio_request_region(PORT_PIT_CH2, 1u, "pit_ch2");
+
+    if (unlikely(!g_pit_ch2_region)) {
+        return;
+    }
+
+    g_ppi_ctrl_region = pmio_request_region(PORT_PPI_CTRL, 1u, "ppi_speaker");
+    
+    if (unlikely(!g_ppi_ctrl_region)) {
+        pmio_release_region(g_pit_ch2_region);
+        g_pit_ch2_region = 0;
+
+        return;
+    }
+
+    g_speaker_ready = 1;
+
+    pc_speaker_stop();
 }
 
 void pc_speaker_play(uint32_t frequency) {
-    uint32_t div;
-    uint8_t tmp;
+    if (unlikely(!g_speaker_ready)) {
+        return;
+    }
 
-    div = 1193180 / frequency;
+    if (unlikely(frequency == 0u || frequency > PIT_FREQUENCY)) {
+        return;
+    }
 
-    outb(0x43, 0xB6);
+    const uint32_t divisor = PIT_FREQUENCY / frequency;
 
-    outb(0x42, (uint8_t)(div));
-    outb(0x42, (uint8_t)(div >> 8));
+    pmio_writeb(PORT_PIT_COMMAND, PIT_CMD_CH2_SQUARE);
 
-    tmp = inb(0x61);
-    if (tmp != (tmp | 3)) {
-        outb(0x61, tmp | 3);
+    pmio_writeb(PORT_PIT_CH2, (uint8_t)(divisor & 0xFFu));
+    pmio_writeb(PORT_PIT_CH2, (uint8_t)((divisor >> 8) & 0xFFu));
+
+    const uint8_t ppi_state = pmio_readb(PORT_PPI_CTRL);
+
+    if ((ppi_state & PPI_SPEAKER_ENABLE) != PPI_SPEAKER_ENABLE) {
+        pmio_writeb(PORT_PPI_CTRL, ppi_state | PPI_SPEAKER_ENABLE);
     }
 }
 
 void pc_speaker_stop(void) {
-    uint8_t tmp = inb(0x61) & 0xFC;
-    outb(0x61, tmp);
+    if (unlikely(!g_speaker_ready)) {
+        return;
+    }
+
+    const uint8_t ppi_state = pmio_readb(PORT_PPI_CTRL);
+
+    pmio_writeb(PORT_PPI_CTRL, ppi_state & PPI_SPEAKER_MASK);
 }
 
 void pc_speaker_beep(void) {
-    pc_speaker_play(1000);
-    
-    simple_wait(1000); 
-    
+    if (unlikely(!g_speaker_ready)) {
+        return;
+    }
+
+    pc_speaker_play(1000u);
+
+    speaker_delay(1000u);
+
     pc_speaker_stop();
 }
 
 void pc_speaker_error(void) {
-    for (int freq = 2000; freq > 300; freq -= 15) {
-        pc_speaker_play(freq);
-        simple_wait(15); 
+    if (unlikely(!g_speaker_ready)) {
+        return;
     }
-    pc_speaker_stop();
-}
 
-void pc_speaker_init(void) {
+    for (uint32_t freq = 2000u; freq > 300u; freq -= 15u) {
+        pc_speaker_play(freq);
+
+        speaker_delay(15u);
+    }
+
     pc_speaker_stop();
 }
--- src/drivers/pc_speaker.h
+++ src/drivers/pc_speaker.h
@@ -1,10 +1,14 @@
 // SPDX-License-Identifier: GPL-2.0
-// Copyright (C) 2025 Yula1234
+// Copyright (C) 2026 Yula1234
 
 #ifndef DRIVERS_PC_SPEAKER_H
 #define DRIVERS_PC_SPEAKER_H
 
 #include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 void pc_speaker_init(void);
 void pc_speaker_play(uint32_t frequency);
@@ -12,5 +16,9 @@ void pc_speaker_stop(void);
 
 void pc_speaker_beep(void);
 void pc_speaker_error(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
