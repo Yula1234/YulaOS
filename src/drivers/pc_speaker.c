ffeaf2e72816f370bb93eb25c982569e19fe7d36
Yula1234 <151495538+Yula1234@users.noreply.github.com>
2026-03-27 16:10:10 +0500

    drivers/speaker: using new delay api

--- src/drivers/pc_speaker.c
+++ src/drivers/pc_speaker.c
@@ -1,9 +1,11 @@
-// SPDX-License-Identifier: GPL-2.0
-// Copyright (C) 2026 Yula1234
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright (C) 2026 Yula1234 */
+
+#include <hal/delay.h>
+#include <hal/pmio.h>
 
 #include <drivers/pc_speaker.h>
 
-#include <hal/pmio.h>
 #include <lib/compiler.h>
 
 #define PIT_FREQUENCY      1193180u
@@ -21,13 +23,6 @@ static pmio_region_t* g_pit_region = 0;
 static pmio_region_t* g_ppi_ctrl_region = 0;
 
 static int g_speaker_ready = 0;
-
-static void speaker_delay(uint32_t units) {
-    const uint32_t iterations = units * 10000u;
-
-    for (volatile uint32_t i = 0u; i < iterations; i++) { }
-}
-
 
 void pc_speaker_init(void) {
     if (unlikely(g_speaker_ready)) {
@@ -108,7 +103,7 @@ void pc_speaker_beep(void) {
 
     pc_speaker_play(1000u);
 
-    speaker_delay(1000u);
+    mdelay(100u);
 
     pc_speaker_stop();
 }
@@ -121,7 +116,7 @@ void pc_speaker_error(void) {
     for (uint32_t freq = 2000u; freq > 300u; freq -= 15u) {
         pc_speaker_play(freq);
 
-        speaker_delay(15u);
+        mdelay(2u);
     }
 
     pc_speaker_stop();
