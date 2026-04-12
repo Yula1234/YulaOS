b90ef6e8e7c4f0d4077abb3cb36d8789d78f63b0
Yula1234 <151495538+Yula1234@users.noreply.github.com>
2026-04-12 21:08:27 +0500

    lib/string: some optimizations

--- src/lib/string.c
+++ src/lib/string.c
@@ -171,76 +171,120 @@ size_t strlcat(char* restrict dst, const char* restrict src, size_t dstsz)
     return dlen + slen;
 }
 
-void* memcpy(void* restrict dst, const void* restrict src, size_t n)
-{
+void* memcpy(void* restrict dst, const void* restrict src, size_t n) {
+    uint32_t d0, d1, d2;
     __asm__ volatile(
         "cld\n\t"
-        "rep movsb"
-        : "+D"(dst), "+S"(src), "+c"(n)
-        :
+        "rep movsl\n\t"
+        "mov %4, %%ecx\n\t"
+        "and $3, %%ecx\n\t"
+        "jz 1f\n\t"
+        "rep movsb\n\t"
+        "1:"
+        : "=&c"(d0), "=&D"(d1), "=&S"(d2)
+        : "0"(n >> 2), "r"(n), "1"(dst), "2"(src)
         : "memory", "cc"
     );
-
     return dst;
 }
 
-void* memmove(void* dst, const void* src, size_t n)
-{
-    if (n == 0 || dst == src)
-    {
+
+void* memmove(void* dst, const void* src, size_t n) {
+    if (unlikely(n == 0 || dst == src)) {
         return dst;
     }
+    
+    uintptr_t d_ptr = (uintptr_t)dst;
+    uintptr_t s_ptr = (uintptr_t)src;
 
-    uint8_t* d = (uint8_t*)dst;
-    const uint8_t* s = (const uint8_t*)src;
-
-    if (d < s || (d >= s + n))
-    {
+    if (d_ptr < s_ptr || d_ptr >= s_ptr + n) {
         return memcpy(dst, src, n);
     }
 
-    d += n;
-    s += n;
+    uint8_t* d = (uint8_t*)dst + n;
 
-    __asm__ volatile(
-        "std\n\t"
-        "rep movsb\n\t"
-        "cld"
-        : "+D"(d), "+S"(s), "+c"(n)
-        :
-        : "memory", "cc"
-    );
+    const uint8_t* s = (const uint8_t*)src + n;
+
+    while (n & 3) {
+        *(--d) = *(--s);
+        n--;
+    }
+
+    uint32_t* d32 = (uint32_t*)d;
+
+    const uint32_t* s32 = (const uint32_t*)s;
+
+    size_t dwords = n >> 2;
+
+    while (dwords--) {
+        *(--d32) = *(--s32);
+    }
 
     return dst;
 }
 
-void* memset(void* dst, int v, size_t n)
-{
-    uint8_t* d = (uint8_t*)dst;
-    uint8_t c = (uint8_t)v;
+
+void* memset(void* dst, int v, size_t n) {
+    uint32_t c32 = (uint8_t)v;
+    
+    c32 |= c32 << 8;
+    c32 |= c32 << 16;
+    
+    uint32_t d0, d1;
 
     __asm__ volatile(
         "cld\n\t"
-        "rep stosb"
-        : "+D"(d), "+c"(n), "+a"(c)
-        :
+        "rep stosl\n\t"
+        "mov %3, %%ecx\n\t"
+        "and $3, %%ecx\n\t"
+        "jz 1f\n\t"
+        "rep stosb\n\t"
+        "1:"
+        : "=&c"(d0), "=&D"(d1)
+        : "a"(c32), "r"(n), "0"(n >> 2), "1"(dst)
         : "memory", "cc"
     );
-
     return dst;
 }
 
-int memcmp(const void* a, const void* b, size_t n)
-{
+
+int memcmp(const void* a, const void* b, size_t n) {
     const uint8_t* pa = (const uint8_t*)a;
     const uint8_t* pb = (const uint8_t*)b;
 
-    for (size_t i = 0; i < n; i++)
-    {
-        if (pa[i] != pb[i])
-        {
-            return (int)pa[i] - (int)pb[i];
+    size_t dwords = n >> 2;
+
+    const uint32_t* pa32 = (const uint32_t*)pa;
+    const uint32_t* pb32 = (const uint32_t*)pb;
+
+    while (dwords--) {
+        if (*pa32 != *pb32) {
+            pa = (const uint8_t*)pa32;
+            pb = (const uint8_t*)pb32;
+
+            for (int i = 0; i < 4; i++) {
+                if (pa[i] != pb[i]) {
+                    return (int)pa[i] - (int)pb[i];
+                }
+            }
         }
+
+        pa32++;
+        pb32++;
+    }
+
+    pa = (const uint8_t*)pa32;
+    pb = (const uint8_t*)pb32;
+
+    size_t bytes = n & 3;
+
+    while (bytes--) {
+        if (*pa != *pb) {
+            return (int)*pa - (int)*pb;
+        }
+
+        pa++;
+        pb++;
     }
 
     return 0;
